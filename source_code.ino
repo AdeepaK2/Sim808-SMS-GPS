c1a095e28a11442f89643af5e994a2b15caef62d
G.G. Adeepa Shashintha Kularathna <105542444+AdeepaK2@users.noreply.github.com>
2024-01-29 18:41:47 +0530

    up1

--- source_code.ino
+++ source_code.ino
@@ -2,15 +2,9 @@
 #include <SoftwareSerial.h>
 
 #define MESSAGE_LENGTH 160
-char message[MESSAGE_LENGTH];
-int messageIndex = 0;
-char MESSAGE[300];
 char lat[12];
 char lon[12];
 char wspeed[12];
-
-char phone[16];
-char datetime[24];
 
 #define PIN_TX 10
 #define PIN_RX 11
@@ -18,7 +12,6 @@ SoftwareSerial mySerial(PIN_TX, PIN_RX);
 DFRobot_SIM808 sim808(&mySerial); // Connect RX,TX,PWR,
 
 void getGPS();
-void readSMS();
 
 void setup()
 {
@@ -35,46 +28,16 @@ void setup()
 
   Serial.println("SIM Init success");
 
-  Serial.println("Init Success, please send SMS message to me!");
+  Serial.println("Init Success, sending location every 30 seconds!");
 }
 
 void loop()
 {
-  //*********** Detecting unread SMS ************************
-  messageIndex = sim808.isSMSunread();
-
-  //*********** At least, there is one UNREAD SMS ***********
-  if (messageIndex > 0)
-  {
-
-    readSMS();
-    getGPS();
-
-    //************* Turn off the GPS power ************
-    sim808.detachGPS();
-
-    Serial.println("Please send SMS message to me!");
-  }
+  // Get GPS location
+  getGPS();
 
   // Wait for 30 seconds before reading GPS again
   delay(30000);
-}
-
-void readSMS()
-{
-  Serial.print("messageIndex: ");
-  Serial.println(messageIndex);
-
-  sim808.readSMS(messageIndex, message, MESSAGE_LENGTH, phone, datetime);
-
-  //***********In order not to fill SIM Memory, it's better to delete the SMS**********
-  sim808.deleteSMS(messageIndex);
-  Serial.print("From number: ");
-  Serial.println(phone);
-  Serial.print("Datetime: ");
-  Serial.println(datetime);
-  Serial.print("Received Message: ");
-  Serial.println(message);
 }
 
 void getGPS()
@@ -92,19 +55,6 @@ void getGPS()
   {
   }
 
-  Serial.print(sim808.GPSdata.year);
-  Serial.print("/");
-  Serial.print(sim808.GPSdata.month);
-  Serial.print("/");
-  Serial.print(sim808.GPSdata.day);
-  Serial.print(" ");
-  Serial.print(sim808.GPSdata.hour);
-  Serial.print(":");
-  Serial.print(sim808.GPSdata.minute);
-  Serial.print(":");
-  Serial.print(sim808.GPSdata.second);
-  Serial.print(":");
-  Serial.println(sim808.GPSdata.centisecond);
   Serial.print("latitude :");
   Serial.println(sim808.GPSdata.lat);
   Serial.print("longitude :");
@@ -119,9 +69,9 @@ void getGPS()
   float lo = sim808.GPSdata.lon;
   float ws = sim808.GPSdata.speed_kph;
 
-  dtostrf(la, 4, 6, lat);    // Put float value of la into char array of lat. 4 = number of digits before decimal sign. 6 = number of digits after the decimal sign.
-  dtostrf(lo, 4, 6, lon);    // Put float value of lo into char array of lon
-  dtostrf(ws, 6, 2, wspeed); // Put float value of ws into char array of wspeed
+  dtostrf(la, 4, 6, lat);
+  dtostrf(lo, 4, 6, lon);
+  dtostrf(ws, 6, 2, wspeed);
 
   Serial.println("Location:");
   Serial.print("Latitude : ");
@@ -135,4 +85,3 @@ void getGPS()
   Serial.println(mapLink);
   Serial.println();
 }
-
